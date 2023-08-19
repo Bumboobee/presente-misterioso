35842bc12916be634597c47205c24fae8b5e64df
José Aparecido <juniorsilva240803@gmail.com>
2023-08-19 18:40:12 -0300

    feature: add the gifts src

--- src/data/memes.ts
+++ src/data/memes.ts
@@ -1,57 +1,37 @@
 const data = [
   {
     meme: "vai responder não, p@t$?",
-    src: 'https://www.tiktok.com/@gleisihoffmannreal/video/7229722352920038662',
+    src: "https://www.tiktok.com/@londrinando/video/6972204567429795077?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
     author: "unknow",
   },
   {
-    meme: "ninguém vai pegar meu telefone!! pegaram meu telefone 😢",
-    src: "",
-    author: "eleicao",
-  },
-  {
-    meme: "padre KELSON 🥰. KELVIN 😕. Padre. Canditado Padre.📿",
-    src: "",
-    author: "eleicao",
-  },
-  {
-    meme: "eu to achando você nervoso 🤨",
-    src: "",
-    author: "eleicao",
-  },
-  {
-    meme: "o senhor não tem medo de ir pro inferno não? 🔥",
-    src: "",
-    author: "tula",
-  },
-  {
-    meme: "canditada Bolsonara 💅",
-    src: "",
-    author: "eleicao",
-  },
-  {
-    meme: "que palhaçada do cara#$@ é essa?",
-    src: "",
-    author: "tula",
-  },
-  {
-    meme: "quem você pensa que é na fila do pão?",
-    src: "",
-    author: "tula",
-  },
-  {
-    meme: "ahan ahhan uhum jaja. você é uma chata. você é insuportavel.",
-    src: "",
-    author: "tula",
-  },
-  {
     meme: "aqui a ração 😋. aqui amora tua raçãozinha 🥘. POR@ VAGABUN#@ 😡",
-    src: "",
+    src: "https://www.tiktok.com/@eduardoposts/video/7216929828203711749?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "unknow",
+  },
+  {
+    meme: "xamdra 🤨, kandra de israel 🕌",
+    src: "https://www.tiktok.com/@sour4u/video/7242082416616033542?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "unknow",
+  },
+  {
+    meme: "nossa 🤨 tudo tão arrumadinho. é 😤. até que a cachorra tem bom gosto 😒",
+    src: "https://www.tiktok.com/@marisobrall_/video/7180453833657634053?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "unknow",
+  },
+  {
+    meme: "eu nao aguento mais esse pais de merdaaa 💩",
+    src: "https://www.tiktok.com/@slashhlg/video/7149268936062782726?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
     author: "unknow",
   },
   {
     meme: "bom dia, ARALHOoo!",
-    src: "",
+    src: "https://www.tiktok.com/@slashhlg/video/7149268936062782726?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733https://www.tiktok.com/@slashhlg/video/7149268936062782726?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "leandrinha",
+  },
+  {
+    meme: "qué um oblox? 🥰 um blox pela anhã? ☀",
+    src: "https://www.tiktok.com/@alan__0003/video/7262740354221296901?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
     author: "leandrinha",
   },
   {
@@ -61,33 +41,53 @@ const data = [
   },
   {
     meme: "derrubei meu c@@ 🤨. meu... meu😟 meu tablet 🤐",
-    src: "",
-    author: "jacquin",
-  },
-  {
-    meme: "xamdra 🤨, kandra de israel 🕌",
-    src: "",
+    src: "https://www.tiktok.com/@scfvrta/video/7149671159041232134?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
     author: "unknow",
   },
   {
-    meme: "eu é que nao vou ficar me estressando 💅, tudo que me estressar esse ano, eu vou descartar da minha vida 🤗",
-    src: "",
+    meme: "ninguém vai pegar meu telefone!! pegaram meu telefone 😢",
+    src: "https://www.tiktok.com/@gleisihoffmannreal/video/7229722352920038662?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "eleicao",
+  },
+  {
+    meme: "padre KELSON 🥰. KELVIN 😕. Padre. Canditado Padre.📿",
+    src: "https://www.tiktok.com/@scfvrta/video/7149671159041232134?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "eleicao",
+  },
+  {
+    meme: "eu to achando você nervoso 🤨",
+    src: "https://www.tiktok.com/@aleatorieditsx/video/7149605777378938117?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "eleicao",
+  },
+  {
+    meme: "canditada Bolsonara 💅",
+    src: "https://www.tiktok.com/@scfvrta/video/7149671159041232134?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "eleicao",
+  },
+  {
+    meme: "o senhor não tem medo de ir pro inferno não? 🔥",
+    src: "https://www.tiktok.com/@aleatorieditsx/video/7149605777378938117?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "eleicao",
+  },
+  {
+    meme: "que palhaçada do cara#$@ é essa?",
+    src: "https://www.tiktok.com/@acervodasrainhas/video/7127985641719188741?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
     author: "tula",
   },
   {
-    meme: "eu nao aguento mais esse pais de merdaaa 💩",
-    src: "",
-    author: "unknow",
+    meme: "quem você pensa que é na fila do pão?",
+    src: "https://www.tiktok.com/@surtostullaluana/video/7239908053582122246?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "tula",
   },
   {
-    meme: "qué um oblox? 🥰 um blox pela anhã? ☀",
-    src: "",
-    author: "leandrinha",
+    meme: "ahan ahhan uhum jaja. você é uma chata. você é insuportavel.",
+    src: "https://www.tiktok.com/@beleledacuca/video/7156636929335758086?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "tula",
   },
   {
-    meme: "nossa 🤨 tudo tão arrumadinho. é 😤. até que a cachorra tem bom gosto 😒",
-    src: "",
-    author: "unknow",
+    meme: "eu é que nao vou ficar me estressando 💅, tudo que me estressar esse ano, eu vou descartar da minha vida 🤗",
+    src: "https://www.tiktok.com/@acervodasrainhas/video/7268743552748784902?is_from_webapp=1&sender_device=pc&web_id=7269114768333833733",
+    author: "tula",
   },
 ];
 
